0c6d90776e8bd0335f005c829d34343c81a62fb9
SID12g <sid12g@naver.com>
2023-09-18 20:41:03 +0900

    making router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Layout from "./components/layout"
 import Home from "./routes/home"
 import Profile from "./routes/profile"
+import CreateAccount from "./routes/create-account"
+import Login from "./routes/login"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
 const router = createBrowserRouter([
   {
@@ -17,13 +21,36 @@ const router = createBrowserRouter([
         element:<Profile/>,
       }
     ]
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/create-account",
+    element:<CreateAccount/>
   }
-])
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box
+  }
+  body{
+    background-color:black;
+    color:white;
+    font-family:'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', 
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 
+    sans-serif;
+  }
+`;
 
 
 function App() {
   return (
     <>
+    <GlobalStyles/>
     <RouterProvider router={router}/>
     </>
   )
