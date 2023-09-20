b8c37f1860a2586155fab147e8ad53505c0ced61
SID12g <sid12g@naver.com>
2023-09-20 18:35:51 +0900

    tweet form screen

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,10 +1,17 @@
+import PostTweetForm from "../components/post-tweet-form";
 import { auth } from "../firebase";
+import {styled} from "styled-components"
+
+const Wrapper = styled.div``;
+
 export default function Home(){
     const logOut = () => {}
     auth.signOut();
     return (
-    <h1>
-        <button onClick={logOut}>Log Out</button>
-    </h1>
+    <>
+    <Wrapper>
+        <PostTweetForm/>
+    </Wrapper>
+    </>
     )
 }
